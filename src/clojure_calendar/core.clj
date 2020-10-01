c5e763e8b4ae4cc1d0f278aa4fd16454764269a6
Rafael Herrera <santoElias65537@gmail.com>
2020-10-01 00:08:26 -0600

    add function which gets the next 28 days, into an array/vector I guess its called in this language

--- src/clojure_calendar/core.clj
+++ src/clojure_calendar/core.clj
@@ -1,7 +1,44 @@
 (ns clojure-calendar.core
   (:gen-class))
 
+(refer-clojure :exclude [range iterate format max min])
+(use 'java-time)
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+(local-date 1999 04 2)
+
+
+(def now (local-date))
+
+(plus now (days 1))
+
+
+
+
+(defn idk
+  []
+  (loop [day (local-date)
+         count 0
+         allDays []]
+    (if (= count 28)
+      allDays
+      (recur (plus day (days 1) (inc count) (conj allDays day))))))
+
+
+
+
+
+(defn test
+  []
+  (loop [count 0
+         day (local-date)
+         other []]
+    (if (= 28 count)
+      other
+      (recur (inc count) (plus day (days 1)) (conj other day)))))
+
+(test)
