f219fccce6669f2774ce5899303e38b73958a782
Rafael Herrera <santoElias65537@gmail.com>
2020-10-07 23:22:33 -0600

    it prints 4 x 7 vector of dates with list of hours. now Clean up, and make it so you can chane any number to true

--- src/clojure_calendar/core.clj
+++ src/clojure_calendar/core.clj
@@ -7,11 +7,12 @@
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println "Hello, World!")
 
 
-(def twentyfour-hours (list 0 1 2 3 4 5 6 7 8 9 10 11 12
-                            13 14 15 16 17 18 19 20 21 22 23))
+ (def twentyfour-hours (list 12 3 6 9)))
+
+; (def twentyfour-hours (list 0))
 
 
 
@@ -25,5 +26,95 @@
       (recur (inc count) (plus day (days 1)) (conj other (str day))))))
 
 
+(defn assignHoursToDates
+  [dates]
+  (map #(hash-map % twentyfour-hours) dates))
+
+(assignHoursToDates (getNext4WeeksDates))
+
+(def calendarV2 (assignHoursToDates (getNext4WeeksDates)))
+
+(def calendarV3 (partition 7 calendarV2))
+
+(println calendarV3)
+
+(print-board calendarV3)
+
+
+
+(defn print-board
+  [board]
+  (doseq [a board]
+   (println a)))
+
+(partition 7 calendarV2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+(defn main
+  []
+  (loop [calendar (assignHoursToDates (getNext4WeeksDates))
+         input "nothing"]
+    (if (= "e" input)
+      (print-board calendar)
+      (recur calendar (read-line)))))
+
+
+
+
+
+
+
+
+
+
+(partition 3 (assignHoursToDates (getNext4WeeksDates)))
+
 
 (map #(hash-map % twentyfour-hours) (getNext4WeeksDates))
+
+
+; TODO
+; INSTEAD OF LIST OF 0 - 23
+; make it hashmap of 0 - 23 being keys, and all being false
+;
+;
+;
+;
