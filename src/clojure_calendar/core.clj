9c4aed25eed68735055a98eeecec4dd322e269f7
Rafael Herrera <santoElias65537@gmail.com>
2020-11-10 14:07:04 -0700

    added to when user enters e prints all trues and not just 4, because I was using mapcat. mapcat does thing by 4.

--- src/clojure_calendar/core.clj
+++ src/clojure_calendar/core.clj
@@ -9,23 +9,6 @@
   [& args]
   (println "Hello, World!"))
 
-; make this a cond instead of (if
-;ex.
-;(cond (= input "e") (exitProgram)
-;      :else do (updateCalendar) (recur))
-;               (recur)
-;
-;
-
-;put do on (= input "e")
-;and println filtered aswell
-;
-;
-;
-;;
-;
-
-
 
 ;(def booked-events
  ; (filter (fn [[dt {:strs [booked?]}]]
@@ -55,12 +38,10 @@
              :nameOfEvent :nothingBookedYet}}))
    (into {})))
 
-(def nextMonthV2 (createDataBase (getNext4WeeksDates)))
+(def nextMonth4 (createDataBase (getNext4WeeksDates)))
 
 
 (defn nextMonthV3 [] (createDataBase (getNext4WeeksDates)))
-
-(println (createDataBase (getNext4WeeksDates)))
 
 ;
 ;todo add onEnter function
@@ -70,158 +51,33 @@
 ;
 ;
 
-
-(defn getInputV3
-  []
-  (loop [input "nothing"
-         allInputs []]
-    (cond
-      (= input "e") (println "exitsg program")
-      :else (do
-              (filter (fn [[dt {:strs [booked?]}]]
-                          booked?)
-                      nextMonthV2)
-              (recur (read-line) (conj allInputs input))))))
-
-
-
-(defn getInputV4
+(defn getInput
   []
   (loop [input "nothing"
          allInputs []]
     (cond
       (= input "e")
-      (do
-        (mapcat println booked-events)
-        (println "exits program"))
+      (doall
+        (apply println (into {} (filter (fn [x] (get-in x [1 :booked?])) nextMonth4)))
+        (println "exits  new"))
       :else (recur (read-line) (conj allInputs input)))))
 
 
+(mapcat println nextMonth4)
+
+(apply println (into {} (filter (fn [x] (get-in x [1 :booked?])) nextMonth4)))
 
 
+;this works
+;why doesnt it return things already in order, wouldnt it do it firsst comee first serve??
 
-
-(def booked-events
- (filter (fn [[dt f
-               dt {:strs [booked?]}]]
-           (println dt))
-         nextMonthV2))
-
-
-(println booked-events)
-
-
-    ;
-    ; (def twentyfour-hours (map #(merge (hash-map % false name "nothing")) (vectorXLong 4)))
-
-
-
-
-
-
-
-(def booked-eventsV2
-  (filter (fn [[dt {:strs [booked?]}]]
-            booked? (println booked?))
-          nextMonthV2))
-
-(println booked-eventsV2)
-
-(let [dt {:keys [booked?]} nextMonthV2]
-  (println dt))
-
-(def bookedV2
-  (filter (fn [[dt {:strs [booked?]}]]
-           (println "fasdf" booked?))
-    nextMonthV2))
-
-(println bookedV2)
-
-(println nextMonthV2)
-
-
-
-
-(filter (fn [[dt {:strs [booked?]} ]] booked?) nextMonthV2)
-
-
-
-
-
-(into {} (filter (fn [[dt {:strs [booked?]} ]] dt) nextMonthV2))
-
-
-
-
-(filter (fn [x] (let [obj x secondLayer obj] (println "whats this layer" (obj :booked?)))) nextMonthV2)
-
-
-
-(filter (fn [x] (let [obj x] (println "whats this layer" ((into {} obj) :booked?)))) nextMonthV2)
-
-
-
-(let [firstLayer nextMonthV2
-      secondLayer (:booked? firstLayer)]
-  (println "idk"  secondLayer))
-
-
-
-
-(filter (fn [x] (let [firstLayer {:strs [booked?]}]
-                 (println secondLayer "idk")))
-   nextMonthV2)
-
-
-
-
-
-
-(let [{{booked :booked?} :keys} nextMonthV2]
- (println "whats happening " booked))
-
-
-(filter (fn [x] (let [{booked :booked?} x] (println booked "fuck") nextMonthV2)))
-;this one works, kinda
-(map
-  (fn
-    [dt]
-    (let [[a b] dt] (println "fuck " b)))
- nextMonthV2)
-
-(fn [[dt {:strs [booked]}]]
-    sd)
-; ;
-;   (filter (fn [[dt {:strs [booked?]}]]
-;             booked? (println booked?))
-;           nextMonthV2)
-
-(nextMonthV3)
-
-(nextMonthV3 {:keys "true"?})
-
-
-(let [{allKeys :keys booked} nextMonthV2]
-  (println "idk" allKeys booked?))
-
-
-(def eventsNotWithinTimeline (atom []))
-
+(map println (into {} (filter (fn [x] (get-in x [1 :booked?])) nextMonth4)))
 
 
 (defn print-board
   [board]
   (doseq [a board]
    (println a)))
-
-
-
-
-
-;change any number to true
-;
-
-
 
 
 
