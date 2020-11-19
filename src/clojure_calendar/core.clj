9ab92e431bb7cfc8792641910270404e18f079b2
Rafael Herrera <santoElias65537@gmail.com>
2020-11-18 22:57:42 -0700

    idk why I thought sorted-map would work, but it does. Now the dates are in order.

--- src/clojure_calendar/core.clj
+++ src/clojure_calendar/core.clj
@@ -19,7 +19,7 @@
   (loop [count 0
          day (local-date)
          other []]
-    (if (= 28 count)
+    (if (= 365 count)
       other
       (recur (inc count) (plus day (days 1)) (conj other (str day))))))
 
@@ -36,8 +36,16 @@
          {x {:booked? false
              :nameOfEvent :nothingBookedYet}}))
    (into {})))
+(println (createDataBase (vectorXLong 28)))
+(createDataBase (vectorXLong 28))
 
-(def nextMonth5 (createDataBase (getNext4WeeksDates)))
+
+(createDataV2 (vectorXLong 5))
+
+(def nextMonth5 (createDataBase (vectorXLong 28)))
+(println nextMonth5)
+(createDataBase (vectorXLong 20))
+
 
 
 (defn bookedEvents
@@ -63,3 +71,42 @@
     (if (or (= dateToChange "e") (= nameOfEvent "e"))
      (println (generate-string (bookedEvents calendar) {:pretty true}))
      (recur (updateCalendarV2 calendar dateToChange nameOfEvent) (read-line) (read-line)))))
+
+
+
+(defn vector-X-Long
+  [x]
+  (loop [itr 0
+         arr []]
+    (if (= itr x)
+     arr
+     (recur (inc itr) (conj arr itr)))))
+
+(vectorXLong 28)
+
+
+
+
+Hey guys, a little confused by this behavior.
+(defn vector-x-long
+  [x]
+  (loop [itr 0
+         arr []]
+    (if (= itr x)
+     arr
+     (recur (inc itr) (conj arr itr)))))
+
+
+(defn create-data
+  [dates]
+  (->> dates
+    (map (fn [x]
+            {x {:booked? false
+                :nameOfEvent :nothingBookedYet}}))
+    (into (sorted-map))))
+
+(def lost (create-data (getNext4WeeksDates)))
+Why doesnt this return the hashmap in order. ex. {0 {}, 1 {}, 2 {} ...}
+Instead it returns what feels like randomness. When I println x inside create-date its in order.
+(println lost)
+How could I get it for def lost to be in order?
