0a2902a4538b57c37a3182f170c59349f0d840b0
Rafael Herrera <santoElias65537@gmail.com>
2020-11-20 15:13:54 -0700

    its coming along nicely. just need it to work when lein run instead of in repl

--- src/clojure_calendar/core.clj
+++ src/clojure_calendar/core.clj
@@ -7,50 +7,9 @@
 (use 'java-time)
 
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
-
-
-
-(defn getNext4WeeksDates
-  []
-  (loop [count 0
-         day (local-date)
-         other []]
-    (if (= 365 count)
-      other
-      (recur (inc count) (plus day (days 1)) (conj other (str day))))))
-
-(defn randomBoolean
-  []
-  (if (= 0 (rand-int 2))
-   true
-   false))
-
-(defn createDataBase
-  [dates]
-  (->> dates
-   (map (fn [x]
-         {x {:booked? false
-             :nameOfEvent :nothingBookedYet}}))
-   (into {})))
-(println (createDataBase (vectorXLong 28)))
-(createDataBase (vectorXLong 28))
-
-
-(createDataV2 (vectorXLong 5))
-
-(def nextMonth5 (createDataBase (vectorXLong 28)))
-(println nextMonth5)
-(createDataBase (vectorXLong 20))
-
-
-
 (defn bookedEvents
   [calendar]
-  (into {}
+  (into (sorted-map)
    (filter
      (fn [x]
        (get-in x [1 :booked?])) calendar)))
@@ -63,50 +22,36 @@
    (update-in calendar [date] assoc :booked? true :nameOfEvent nameOfEvent)))
 
 
+(defn getNext4WeeksDates
+  []
+  (loop [count 0
+         day (local-date)
+         other []]
+    (if (= 28 count)
+      other
+      (recur (inc count) (plus day (days 1)) (conj other (str day))))))
+
+
+(defn create-data
+   [dates]
+   (->> dates
+     (map (fn [x]
+             {x {:booked? false
+                 :nameOfEvent :nothingBookedYet}}))
+     (into (sorted-map))))
+
+
 (defn getInput
   []
-  (loop [calendar (createDataBase (getNext4WeeksDates))
+  (loop [calendar (create-data (getNext4WeeksDates))
          dateToChange ""
          nameOfEvent ""]
     (if (or (= dateToChange "e") (= nameOfEvent "e"))
      (println (generate-string (bookedEvents calendar) {:pretty true}))
-     (recur (updateCalendarV2 calendar dateToChange nameOfEvent) (read-line) (read-line)))))
+     (recur (updateCalendar calendar dateToChange nameOfEvent) (read-line) (read-line)))))
 
 
-
-(defn vector-X-Long
-  [x]
-  (loop [itr 0
-         arr []]
-    (if (= itr x)
-     arr
-     (recur (inc itr) (conj arr itr)))))
-
-(vectorXLong 28)
-
-
-
-
-Hey guys, a little confused by this behavior.
-(defn vector-x-long
-  [x]
-  (loop [itr 0
-         arr []]
-    (if (= itr x)
-     arr
-     (recur (inc itr) (conj arr itr)))))
-
-
-(defn create-data
-  [dates]
-  (->> dates
-    (map (fn [x]
-            {x {:booked? false
-                :nameOfEvent :nothingBookedYet}}))
-    (into (sorted-map))))
-
-(def lost (create-data (getNext4WeeksDates)))
-Why doesnt this return the hashmap in order. ex. {0 {}, 1 {}, 2 {} ...}
-Instead it returns what feels like randomness. When I println x inside create-date its in order.
-(println lost)
-How could I get it for def lost to be in order?
+(defn -main
+   "I don't do a whole lot ... yet."
+   [& args]
+   (getInput))
